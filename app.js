5e0a24ebecd1ad298c84981c65e54edb4646b006
Alaa Seada <alaaseada87@gmail.com>
2024-07-05 19:43:36 +0300

    Remove Netlify dependencies

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
-const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
-const router = express.Router()
 
 const { get404ErrorPage } = require('./controllers/errors')
 const app = express()
@@ -14,12 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use('/.netlify/functions/app', router)
 app.use('/admin', adminRouter)
 app.use(shopRouter)
 
 app.use(get404ErrorPage)
 
-// app.listen(3000, () => console.log('Server is running on port 3000'))
-
-module.exports.handler = serverless(app)
+app.listen(3000, () => console.log('Server is running on port 3000'))
